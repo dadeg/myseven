4710c487fd1f4d2b57f23d88684889e366fcaf66
Dan DeGreef <dan.degreef@gmail.com>
2014-05-14 20:46:38 -0500

    changed db info

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravelbase',
+			'database'  => 'myseven',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
